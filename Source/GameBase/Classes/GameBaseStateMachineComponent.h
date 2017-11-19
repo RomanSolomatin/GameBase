91cb3f805ce53285b451337408e1203dfe74b33f
bacinsky <peter@bacinsky.sk>
2017-11-19 11:19:03 +0100

    Added state machine component

---
+++ Source/GameBase/Classes/GameBaseStateMachineComponent.h
@@ -0,0 +1,133 @@
+﻿// Copyright (c) 2017, Peter Bačinský <peter@bacinsky.sk>
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "GameBaseStateMachineComponent.generated.h"
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnEnterState, uint8, State);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnExitState, uint8, State);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnTickState, uint8, State);
+
+/**
+ * State Machine Component
+ */
+UCLASS(Blueprintable, ClassGroup = (Custom,StateMachine), meta = (BlueprintSpawnableComponent))
+class GAMEBASE_API UGameBaseStateMachineComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this component's properties
+	UGameBaseStateMachineComponent();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+	// Component tick delta seconds
+	UPROPERTY(BlueprintReadOnly, Category = StateMachine)
+	float DeltaSeconds;
+
+	// Network active state
+	UPROPERTY(BlueprintReadOnly, Replicated, Category = StateMachine)
+	uint8 NetActiveState;
+
+	// Active state
+	UPROPERTY(BlueprintReadOnly, Category = StateMachine)
+	uint8 ActiveState;
+
+	// Active duration
+	UPROPERTY(BlueprintReadOnly, Category = StateMachine)
+	float ActiveStateSeconds;
+
+	// Last state
+	UPROPERTY(BlueprintReadOnly, Category = StateMachine)
+	uint8 LastState;
+
+public:
+	// Activates component
+	void Activate(bool bReset = false) override;
+
+	// Deactivates component
+	void Deactivate() override;
+
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+	// Replicated properties
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+
+	// On enter state
+	UPROPERTY(BlueprintAssignable, Category = StateMachine)
+	FOnEnterState OnEnterState;
+
+	// On exit state
+	UPROPERTY(BlueprintAssignable, Category = StateMachine)
+	FOnExitState OnExitState;
+
+	// On tick state
+	UPROPERTY(BlueprintAssignable, Category = StateMachine)
+	FOnTickState OnTickState;
+
+	// On state initialization
+	UFUNCTION(BlueprintImplementableEvent, DisplayName = InitState, Category = StateMachine)
+	void EventInitState();
+
+	// On enter state
+	UFUNCTION(BlueprintImplementableEvent, DisplayName = EnterState, Category = StateMachine)
+	void EventEnterState();
+
+	// On exit state
+	UFUNCTION(BlueprintImplementableEvent, DisplayName = ExitState, Category = StateMachine)
+	void EventExitState();
+
+	// Tick state
+	UFUNCTION(BlueprintImplementableEvent, DisplayName = TickState, Category = StateMachine)
+	void EventTickState();
+
+	// Before enter state
+	UFUNCTION(BlueprintNativeEvent, Category = StateMachine)
+	bool CanEnterState() const;
+
+	// Before exit state
+	UFUNCTION(BlueprintNativeEvent, Category = StateMachine)
+	bool CanExitState() const;
+
+	// Return state
+	UFUNCTION(BlueprintCallable, BlueprintPure, Category = StateMachine)
+	uint8 GetState();
+
+	// Return last state
+	UFUNCTION(BlueprintCallable, BlueprintPure, Category = StateMachine)
+	uint8 GetLastState();
+
+	// Enter state
+	UFUNCTION(BlueprintCallable, Category = StateMachine)
+	bool EnterState(uint8 NewState, bool bLock = false);
+
+	// Toggle state
+	UFUNCTION(BlueprintCallable, Category = StateMachine)
+	bool ToggleState(uint8 NewState, bool bLock = false);
+
+	// Lock with delay
+	UFUNCTION(BlueprintCallable, Category = StateMachine)
+	void LockDelay(float Seconds);
+
+	// Lock
+	UFUNCTION(BlueprintCallable, Category = StateMachine)
+	void Lock();
+
+	// Unlocks
+	UFUNCTION(BlueprintCallable, Category = StateMachine)
+	void UnLock();
+
+	// Lock
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = StateMachine)
+	bool bLocked;
+
+	// Tick interval (-1 no tick)
+	UPROPERTY(EditAnywhere, Category = Tick)
+	float TickInterval;
+};
---
+++ Source/GameBase/Private/GameBaseStateMachineComponent.cpp
@@ -0,0 +1,165 @@
+﻿// Copyright (c) 2017, Peter Bačinský <peter@bacinsky.sk>
+
+#include "GameBaseStateMachineComponent.h"
+#include "UnrealNetwork.h"
+#include "Kismet/KismetSystemLibrary.h"
+#include "Engine/World.h"
+#include "TimerManager.h"
+
+
+// Sets default values for this component's properties
+UGameBaseStateMachineComponent::UGameBaseStateMachineComponent()
+{
+	PrimaryComponentTick.bCanEverTick = (TickInterval >= 0);
+	ActiveState = 0;
+	bAutoActivate = true;
+	bLocked = false;
+	bReplicates = true;
+}
+
+// Activates component
+void UGameBaseStateMachineComponent::Activate(bool bReset)
+{
+	Super::Activate(bReset);
+	bLocked = false;
+}
+
+// Deactivates component
+void UGameBaseStateMachineComponent::Deactivate()
+{
+	Super::Deactivate();
+	bLocked = true;
+}
+
+
+// Called when the game starts
+void UGameBaseStateMachineComponent::BeginPlay()
+{
+	Super::BeginPlay();
+	bLocked = !IsActive();
+	PrimaryComponentTick.TickInterval = TickInterval;
+	SetComponentTickEnabled(TickInterval >= 0);
+
+	// Enter first state
+	ActiveState = -1;
+	NetActiveState = -1;
+	EnterState(0);
+}
+
+
+// Called every frame
+void UGameBaseStateMachineComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	DeltaSeconds = DeltaTime;
+	ActiveStateSeconds += DeltaTime;
+
+	this->EventTickState();
+	OnTickState.Broadcast(ActiveState);
+
+	// TODO update net state
+	if (ActiveState != NetActiveState)
+		EnterState(NetActiveState);
+}
+
+
+// Lock with delay
+void UGameBaseStateMachineComponent::LockDelay(float Seconds)
+{
+	Lock();
+	FTimerHandle LockDelayTimer;
+	GetWorld()->GetTimerManager().SetTimer(LockDelayTimer, this, &UGameBaseStateMachineComponent::UnLock, Seconds);
+}
+
+
+// Locks
+void UGameBaseStateMachineComponent::Lock()
+{
+	bLocked = true;
+}
+
+
+// Unlocks
+void UGameBaseStateMachineComponent::UnLock()
+{
+	bLocked = false;
+}
+
+
+// Return state
+uint8 UGameBaseStateMachineComponent::GetState()
+{
+	return ActiveState;
+}
+
+
+// Return last state
+uint8 UGameBaseStateMachineComponent::GetLastState()
+{
+	return LastState;
+}
+
+
+// Enter state
+bool UGameBaseStateMachineComponent::EnterState(uint8 NewState, bool bLock)
+{
+	if (bLocked || NewState == ActiveState) return false;
+
+	bool CanEnterState = true;
+
+	if (-1 != ActiveState)
+	{
+		this->EventExitState();
+		OnExitState.Broadcast(ActiveState);
+		LastState = ActiveState;
+
+		bool CanEnterState = this->CanEnterState();
+	}
+
+	if (CanEnterState)
+	{
+		if (bLock) bLocked = true;
+
+		ActiveState = NewState;
+		NetActiveState = NewState;
+		ActiveStateSeconds = 0;
+		this->EventEnterState();
+		OnEnterState.Broadcast(ActiveState);
+		return true;
+	}
+
+	return false;
+}
+
+
+// Toggles state
+bool UGameBaseStateMachineComponent::ToggleState(uint8 NewState, bool bLock)
+{
+	if (NewState == ActiveState)
+		return EnterState(LastState);
+
+	return EnterState(NewState);
+}
+
+
+// Before enter state, default implementation
+bool UGameBaseStateMachineComponent::CanEnterState_Implementation() const
+{
+	return true;
+}
+
+
+// Before exit state, default implementation
+bool UGameBaseStateMachineComponent::CanExitState_Implementation() const
+{
+	return true;
+}
+
+
+// Set network properties
+void UGameBaseStateMachineComponent::GetLifetimeReplicatedProps(TArray< FLifetimeProperty > & OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+	DOREPLIFETIME(UGameBaseStateMachineComponent, NetActiveState);
+}
